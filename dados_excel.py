ccdf81bde1d09a27aeb5bd2ab12408a16fa6f39d
GuOliv2306 <gugu.os84@gmail.com>
2024-06-22 15:03:37 -0300

    arquico dados_excel que transforma o dados_produtos.json para excel.

--- dados_excel.py
+++ dados_excel.py
@@ -1,2 +1,27 @@
+import json
+import pandas as pd
 
-hggm
+#ler o .json dos produtos
+with open('dados_produtos.json', 'r', encoding='utf-8') as file:
+    produtos = json.load(file)
+
+#retirando as descrições do excel
+if isinstance(produtos, list):
+    for item in produtos:
+        if 'depoimentos' in item:
+            del item['depoimentos']
+else:
+    print("Estrutura do JSON não é uma lista de dicionários.")
+
+#conversão para data frame pandas
+df=pd.DataFrame(produtos)
+
+#data frame para excel
+df.to_excel('dados_produtos.xlsx', index=False)
+print('conversão para excel concluída')
+
+
+
+
+
+
